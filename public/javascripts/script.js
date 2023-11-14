dfa4f0d625046f1c1458bf3cba4353de0fe4951e
John Steel <john@jskw.dev>
2023-11-13 21:02:44 -0500

    init

---
+++ public/javascripts/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const plusOneBtn = document.getElementById('plusOneBtn');
+    const minusOneBtn = document.getElementById('minusOneBtn');
+    const count = document.getElementById('count');
+    let socket  = new WebSocket('ws://localhost:3000/ws');
+    let room = window.location.hash.substring(1) || generateUUID();
+
+    socket.onopen = function() {
+        plusOneBtn.disabled = false;
+        minusOneBtn.disabled = false;
+        handleMessage('Connected');
+        socket.send("Subscribe: " + room)
+        window.location.hash = room;
+    };
+
+    socket.onmessage = function(event) {
+        handleMessage(`${event.data}`);
+    };
+
+    socket.onclose = function() {
+        plusOneBtn.disabled = true;
+        minusOneBtn.disabled = true;
+        handleMessage('Disconnected');
+    };
+
+    socket.onerror = function() {
+        handleMessage('Error occurred');
+    };
+
+    plusOneBtn.addEventListener('click', () => {
+        if (socket) {
+            socket.send('+1 ' + room);
+        }
+        handleMessage('+1')
+    });
+
+    minusOneBtn.addEventListener('click', () => {
+        if (socket) {
+            socket.send('-1 ' + room);
+        }
+        handleMessage('-1')
+    });
+
+    function handleMessage(message) {
+        const messageElement = document.createElement('li');
+        messageElement.textContent = message;
+        console.log(message);
+        const value = parseInt(message);
+        if (isFinite(value)) {
+            count.innerText = (parseInt(count.innerText) + value).toString()
+        }
+    }
+});
+
+function getRandomHexDigit() {
+    return (crypto.getRandomValues(new Uint8Array(1))[0] % 16).toString(16);
+}
+
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, char =>
+        (char === 'x' ? getRandomHexDigit() : (getRandomHexDigit() & 0x3 | 0x8).toString(16))
+    );
+}
