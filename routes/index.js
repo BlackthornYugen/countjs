c448a638b7908f6c8282a6ac42b931e009c84765
John Steel <john@jskw.dev>
2023-11-15 20:29:34 -0500

    halloween layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'WebSocket Client' });
+  let params = { title: "Websocket Counter" };
+
+  if (req.headers["host"].startsWith("trick")) {
+    params.title = "Trick or Treater Counter"
+    params.subdomain = "trick"
+  }
+
+  res.render('index', params);
 });
 
 module.exports = router;
